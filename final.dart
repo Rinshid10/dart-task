7e3e2b25404011241d191b9ddec95a52038e4959
RINSHID CH <rinshidch10@gmail.com>
2024-09-11 23:28:26 -0700

    Add files via upload

---
+++ final.dart
@@ -0,0 +1,8 @@
+void main (){
+   const String a = 'rinshid';
+   final String b='ch';
+    // a='a';    this re assain or re decleare   a new value  is not possible
+    
+    // b='k';
+   print(a);
+}
