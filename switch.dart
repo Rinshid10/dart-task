fb4addbc87693b44f8af185683121c9330520199
RINSHID CH <rinshidch10@gmail.com>
2024-09-11 23:29:18 -0700

    Add files via upload

---
+++ switch.dart
@@ -0,0 +1,12 @@
+void main (){
+  var a = 2;
+  switch (a){
+     case 1:
+      print('today is sunday '); break;
+      case 2:
+      print('today is monday ');
+      default: 
+      print('noo0');
+  }
+        
+}
